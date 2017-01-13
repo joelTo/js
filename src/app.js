2dd800bd99b88e060574e47e97d5b434259ae7e1
joel T <joel.torib@gmail.com>
2017-01-13 10:12:36 +0100

    apres import

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import { RecipesService } from './services/recipes.service';
+import { PizzeriaService } from './services/pizzeria.service';
+
 const recipesService = new RecipesService();
 const pizzeriaService = new PizzeriaService(recipesService);
 
